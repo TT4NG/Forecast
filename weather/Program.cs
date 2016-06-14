0e0e8d8898836421c5232c25f34ab55c490c2f1a
TT4NG <t4ngk1k@gmail.com>
2016-06-14 11:52:29 -0500

    new forecast application

---
+++ weather/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weather fore = new Weather();
+            Console.WriteLine("Weather forecast for the week: ");
+            fore.Cast();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ weather/Weather.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace weather
+{
+    class Weather
+    {
+        enum day { Sun, Mon, Tues, Wed, Thurs, Fri, Sat};
+        enum weather { cloudy, sunny, rainy, muggy, foggy}
+        public void Cast()
+        {
+            string[] days = Enum.GetNames(typeof(day));
+            string[] weather = Enum.GetNames(typeof(weather));
+            Random random = new Random();
+            
+
+            for (int i = 0; i<days.Length;i++)
+            {
+                int rand = random.Next(0, weather.Length);
+                Console.WriteLine(days[i] + " - " + weather[rand]);
+            }
+        }
+    }
+}
